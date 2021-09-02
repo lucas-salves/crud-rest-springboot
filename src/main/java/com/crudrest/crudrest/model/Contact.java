687fa4e41d98cc932c1256a74a40135c44e26e56
lucas-salves <lucas.salves@terra.com>
2021-09-02 19:00:53 -0300

    add: creation of the Contact model class

---
+++ src/main/java/com/crudrest/crudrest/model/Contact.java
@@ -0,0 +1,23 @@
+package com.crudrest.crudrest.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Contact {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	private String email;
+	private String phone;
+}
