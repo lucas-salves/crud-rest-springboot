0e256857e5beb708b196f338e57a27bb62c43bdd
lucas-salves <lucas.salves@terra.com>
2021-09-02 23:58:44 -0300

    add: creation of the ContactController

---
+++ src/main/java/com/crudrest/crudrest/controller/ContactController.java
@@ -0,0 +1,18 @@
+package com.crudrest.crudrest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.crudrest.crudrest.repository.ContactRepository;
+
+@RestController
+@RequestMapping("/contacts")
+public class ContactController {
+	
+	
+	
+	
+	
+	
+}
