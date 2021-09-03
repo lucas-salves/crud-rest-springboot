60894f95daac2406fbb926fa406d251566f4ea56
lucas-salves <lucas.salves@terra.com>
2021-09-03 00:44:09 -0300

    add: adding the GetMapping annotation to the findAll controller method

--- src/main/java/com/crudrest/crudrest/controller/ContactController.java
+++ src/main/java/com/crudrest/crudrest/controller/ContactController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +21,11 @@ public class ContactController {
 	private ContactServiceImpl contactService;
 	
 	
-	
+	@GetMapping
 	public List<Contact> findAll(Model model) {
 		return contactService.findAllContacts();
 	}
+	
+	
 	
 }
