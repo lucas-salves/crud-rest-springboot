5368ce7f5d0f4cf1ef3f1db99ec136ed124863d6
lucas-salves <lucas.salves@terra.com>
2021-09-03 00:41:27 -0300

    add: creation of the ContactController findAll() method

--- src/main/java/com/crudrest/crudrest/controller/ContactController.java
+++ src/main/java/com/crudrest/crudrest/controller/ContactController.java
@@ -1,18 +1,28 @@
 package com.crudrest.crudrest.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.crudrest.crudrest.repository.ContactRepository;
+import com.crudrest.crudrest.model.Contact;
+import com.crudrest.crudrest.service.ContactService;
+import com.crudrest.crudrest.service.ContactServiceImpl;
 
 @RestController
 @RequestMapping("/contacts")
 public class ContactController {
 	
 	
+	@Autowired
+	private ContactServiceImpl contactService;
 	
 	
 	
+	public List<Contact> findAll(Model model) {
+		return contactService.findAllContacts();
+	}
 	
 }
